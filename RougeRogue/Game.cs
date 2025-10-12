aa8c7fc3d31dce2231cd11e2300bfa1e0f96aeba
mutms7 <welcomehomemutms7@gmail.com>
2025-10-12 16:59:18 -0400

    w

--- RougeRogue/Game.cs
+++ RougeRogue/Game.cs
@@ -7,8 +7,27 @@ namespace RogueSharpV3Tutorial
         // The screen height and width are in number of tiles
         private static readonly int _screenWidth = 100;
         private static readonly int _screenHeight = 70;
-
         private static RLRootConsole _rootConsole;
+
+        // The map console takes up most of the screen and is where the map will be drawn
+        private static readonly int _mapWidth = 80;
+        private static readonly int _mapHeight = 48;
+        private static RLConsole _mapConsole;
+
+        // Below the map console is the message console which displays attack rolls and other information
+        private static readonly int _messageWidth = 80;
+        private static readonly int _messageHeight = 11;
+        private static RLConsole _messageConsole;
+
+        // The stat console is to the right of the map and display player and monster stats
+        private static readonly int _statWidth = 20;
+        private static readonly int _statHeight = 70;
+        private static RLConsole _statConsole;
+
+        // Above the map is the inventory console which shows the players equipment, abilities, and items
+        private static readonly int _inventoryWidth = 80;
+        private static readonly int _inventoryHeight = 11;
+        private static RLConsole _inventoryConsole;
 
         public static void Main()
         {
@@ -19,6 +38,12 @@ namespace RogueSharpV3Tutorial
             // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
             _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight,
               8, 8, 1f, consoleTitle);
+
+            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _statConsole = new RLConsole(_statWidth, _statHeight);
+            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
             // Set up a handler for RLNET's Update event
             _rootConsole.Update += OnRootConsoleUpdate;
             // Set up a handler for RLNET's Render event
@@ -30,7 +55,33 @@ namespace RogueSharpV3Tutorial
         // Event handler for RLNET's Update event
         private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
         {
-            _rootConsole.Print(10, 10, "It worked!", RLColor.White);
+            // Set background color and text for each console 
+            // so that we can verify they are in the correct positions
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, RLColor.LightRed);
+            _mapConsole.Print(1, 1, "Map", RLColor.White);
+
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
+            _messageConsole.Print(1, 1, "Messages", RLColor.White);
+
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, RLColor.Brown);
+            _statConsole.Print(1, 1, "Stats", RLColor.White);
+
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, RLColor.Cyan);
+            _inventoryConsole.Print(1, 1, "Inventory", RLColor.White);
+
+
+            // Blit the sub consoles to the root console in the correct locations
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight,
+              _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight,
+              _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight,
+              _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight,
+              _rootConsole, 0, 0);
+
+            // Tell RLNET to draw the console that we set
+            _rootConsole.Draw();
         }
 
         // Event handler for RLNET's Render event
