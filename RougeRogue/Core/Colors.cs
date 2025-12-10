e28a977d1cd0c97aaf99ca5b76eca88aed976287
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-10 17:01:12 -0500

    m

--- RougeRogue/Core/Colors.cs
+++ RougeRogue/Core/Colors.cs
@@ -20,5 +20,7 @@ namespace RougeRogue.Core
         public static RLColor WallFov = Swatch.SecondaryLighter;
 
         public static RLColor TextHeading = Swatch.DbLight;
+
+        public static RLColor Player = Swatch.DbLight;
     }
 }
