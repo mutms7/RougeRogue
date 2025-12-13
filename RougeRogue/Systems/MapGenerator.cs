23050a8c9ec806527136667d80d6f9dfb2b16039
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-13 12:33:53 -0500

    m

--- RougeRogue/Systems/MapGenerator.cs
+++ RougeRogue/Systems/MapGenerator.cs
@@ -35,7 +35,19 @@ namespace RougeRogue.Systems
         // Generate a new map that is a simple open floor with walls around the outside
         public DungeonMap CreateMap()
         {
-            
+            // all cell properties are false
+            _map.Initialize(_width, _height);
+
+            // try to place as many rooms as maxrooms
+            for (int r = 0; r < _maxRooms; r++)
+            {
+                //size and pos of room randomly
+                int roomWidth = Game.Random.Next(_roomMinSize, _roomMaxSize);
+                int roomHeight = Game.Random.Next(_roomMinSize, _roomMaxSize);
+                int roomXPosition = Game.Random.Next(0, _width - roomWidth - 1);
+                int roomYPosition = Game.Random.Next(0, _height - roomHeight - 1);
+
+            }
         }
     }
 }
