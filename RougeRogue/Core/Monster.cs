e2b99058822f3382aaa123769c929dce6253c1c4
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-24 01:19:38 -0800

    m

---
+++ RougeRogue/Core/Monster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RougeRogue.Core
+{
+    public class Monster : Actor
+    {
+        // Empty for now, we will add functionality later
+    }
+}
