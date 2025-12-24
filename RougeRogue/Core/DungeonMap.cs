0f80fd4655a9f877107039ee36b6afaf5853eb12
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-24 01:34:52 -0800

    m

--- RougeRogue/Core/DungeonMap.cs
+++ RougeRogue/Core/DungeonMap.cs
@@ -7,11 +7,13 @@ namespace  RougeRogue.Core
     public class DungeonMap : Map
     {
         public List<Rectangle> Rooms;
+        private readonly List<Monster> _monsters;
 
         public DungeonMap()
         {
             // Initialize the list of rooms when we create a new DungeonMap
             Rooms = new List<Rectangle>();
+            _monsters = new List<Monster>();
         }
         // This method will be called any time we move the player to update field-of-view
         public void UpdatePlayerFieldOfView()
@@ -112,7 +114,47 @@ namespace  RougeRogue.Core
             UpdatePlayerFieldOfView();
         }
 
-        
-        
-    }
+
+        public void AddMonster(Monster monster)
+        {
+            _monsters.Add(monster);
+            // cell not walkable
+            SetIsWalkable(monster.X, monster.Y, false);
+        }
+
+
+        public Point GetRandomWalkableLocationInRoom(Rectangle room)
+        {
+            if (DoesRoomHaveWalkableSpace(room))
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int x = Game.Random.Next(1, room.Width - 2) + room.X;
+                    int y = Game.Random.Next(1, room.Height - 2) + room.Y;
+                    if (IsWalkable(x, y))
+                    {
+                        return new Point(x, y);
+                    }
+                }
+                
+            }
+            return null;
+        }
+
+        public bool DoesRoomHaveWalkableSpace(Rectangle room)
+        {
+            for (int i = 1; i < room.Width-2; i++)
+            {
+                for (int j = 1; j < room.Height-2; j++)
+                {
+                    if (IsWalkable(room.X + i, room.Y + j))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
 }
