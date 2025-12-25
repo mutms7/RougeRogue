248e72acff2c490887417112a02e65c2ee03ab82
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-25 13:23:39 -0800

    ,

--- RougeRogue/Core/Colors.cs
+++ RougeRogue/Core/Colors.cs
@@ -5,18 +5,18 @@ namespace RougeRogue.Core
     public class Colors
     {
         public static RLColor FloorBackground = RLColor.Black;
-        public static RLColor Floor = Swatch.AlternateDarkest;
+        public static RLColor Floor = Swatch.SecondaryDarkest;
         public static RLColor FloorBackgroundFov = Swatch.DbDark;
         public static RLColor FloorFov = Swatch.Alternate;
         public static RLColor WallBackground = Swatch.SecondaryDarkest;
-        public static RLColor Wall = Swatch.Secondary;
+        public static RLColor Wall = Swatch.SecondaryDarker;
         public static RLColor WallBackgroundFov = Swatch.SecondaryDarker;
-        public static RLColor WallFov = Swatch.SecondaryLighter;
-        public static RLColor Player = Swatch.DbLight;
+        public static RLColor WallFov = Swatch.Secondary;
+        public static RLColor Player = Swatch.PrimaryLightest;
         public static RLColor TextHeading = RLColor.White;
         public static RLColor Text = Swatch.DbLight;
         public static RLColor Gold = Swatch.DbSun;
 
-        public static RLColor KoboldColor = Swatch.DbOldBlood;
+        public static RLColor KoboldColor = Swatch.Compliment;
     }
 }
