f62a57555dbf8778d0210286fcf924c704377b7c
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-31 17:55:35 -0800

    m

--- RougeRogue/Core/Behaviors/StandardMoveAndAttack.cs
+++ RougeRogue/Core/Behaviors/StandardMoveAndAttack.cs
@@ -27,41 +27,30 @@ namespace RougeRogue.Core.Behaviors
                 if (monsterFov.IsInFov(player.X, player.Y))
                 {
                     Game.MessageLog.Add($"{monster.Name} has noticed {player.Name}");
-                }
-            }
-        }
-    }
-    public class StandardMoveAndAttack : IBehavior
-    {
-        public bool Act(Monster monster, CommandSystem commandSystem)
-        {
-            DungeonMap dungeonMap = Game.DungeonMap;
-            Player player = Game.Player;
-            FieldOfView monsterFov = new FieldOfView(dungeonMap);
-
-            // If the monster has not been alerted, compute a field-of-view 
-            // Use the monster's Awareness value for the distance in the FoV check
-            // If the player is in the monster's FoV then alert it
-            // Add a message to the MessageLog regarding this alerted status
-            if (!monster.TurnsAlerted.HasValue)
-            {
-                monsterFov.ComputeFov(monster.X, monster.Y, monster.Awareness, true);
-                if (monsterFov.IsInFov(player.X, player.Y))
-                {
-                    Game.MessageLog.Add($"{monster.Name} is eager to fight {player.Name}");
                     monster.TurnsAlerted = 1;
                 }
             }
 
             if (monster.TurnsAlerted.HasValue)
             {
-                // Before we find a path, make sure to make the monster and player Cells walkable
+                // before finding path, monster and player cells should be walkable
                 dungeonMap.SetIsWalkable(monster.X, monster.Y, true);
                 dungeonMap.SetIsWalkable(player.X, player.Y, true);
 
                 PathFinder pathFinder = new PathFinder(dungeonMap);
                 Path path = null;
 
+                try
+                {
+                    path = pathFinder.ShortestPath(
+                        dungeonMap.GetCell(monster.X, monster.Y),
+                        dungeonMap.GetCell(player.X, player.Y));
+                        
+                }
+            }
+        }
+    }
+   
                 try
                 {
                     path = pathFinder.ShortestPath(
