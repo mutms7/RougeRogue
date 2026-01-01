4cd713efe2fc84ab71e63eee4b10cff03b1cdb26
William Chenyin <welcomehomemutms7@gmail.com>
2025-12-31 17:01:58 -0800

    m

---
+++ RougeRogue/Interfaces/IBehavior.cs
@@ -0,0 +1,15 @@
+﻿using RougeRogue.Core;
+using RougeRogue.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RougeRogue.Interfaces
+{
+    public interface IBehavior
+    {
+        bool Act(Monster monster, CommandSystem commandSystem);
+    }
+}
